52355e537cc8ee2bb402c5863d107f796eb5629e
BenKung <moon791017@gmail.com>
2019-09-29 21:27:18 +0800

    OK

--- Crawler/src/index.ts
+++ Crawler/src/index.ts
@@ -1,2 +1,15 @@
 import { ClientConfig, crawlClient } from './Helpers/crawlerHelper';
-console.log('a')
+const getContent=async (url:string)=>{
+    let clientConfig: ClientConfig = new ClientConfig();
+    clientConfig.ignoreStatic = true;
+    clientConfig.url = url;
+    const { browser, page, $ } = await crawlClient(clientConfig);
+    //browser.close();
+    page.close();
+    return $
+}
+(async () => {
+    const htmlContent=await getContent(`https://ithome.com.tw/news/133290`);
+    console.log(htmlContent('.field-items').text())
+    
+})()
