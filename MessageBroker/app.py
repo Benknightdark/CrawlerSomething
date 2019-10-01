d7ab090c6fd0112ed99d1082c321a63365fe69cf
Ben Kung <ben_kung@infofab.com>
2019-10-01 16:38:03 +0800

    OK

--- MessageBroker/app.py
+++ MessageBroker/app.py
@@ -1,14 +1,10 @@
 import pika
-credentials = pika.PlainCredentials('rabbitmq', 'rabbitmq')
-parameters = pika.ConnectionParameters('localhost',
-                                       5672,
-                                       '/',
-                                       credentials)
+parameters= pika.URLParameters('amqp://rabbitmq:rabbitmq@localhost:5672/')
 connection = pika.BlockingConnection(parameters)
 channel = connection.channel()
 channel.queue_declare(queue='crawl-url')
 channel.basic_publish(exchange='',
                       routing_key='crawl-url',
-                      body='https://www.ithome.com.tw/news/133339')
+                      body='https://www.ithome.com.tw/news/133335')
 print(" [x] Sent 'Hello World!'")
 connection.close()
