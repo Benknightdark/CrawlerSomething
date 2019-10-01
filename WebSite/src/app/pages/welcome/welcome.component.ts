72e481403bd87114d09cda9e31c717c850197440
Ben Kung <ben_kung@infofab.com>
2019-10-01 19:21:05 +0800

    OK

--- WebSite/src/app/pages/welcome/welcome.component.ts
+++ WebSite/src/app/pages/welcome/welcome.component.ts
@@ -8,23 +8,33 @@ import { WelcomeService } from './welcome.service';
 })
 export class WelcomeComponent implements OnInit {
   url: string;
+  dataSet
   constructor(private service: WelcomeService) { }
 
   ngOnInit() {
     this.service.getData().subscribe(a => {
+      this.dataSet=a;
       console.log(a)
     })
   }
   onClick() {
     const data = { url: this.url };
-    this.service.addData(data).subscribe(a => {
-      console.log(a)
-      alert("已開始爬取")
-    },
-      error => {
-        alert(error.message)
-      }
-    )
+    const re = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
+    if (re.test(this.url)) {
+      console.log("Valid");
+      this.service.addData(data).subscribe(a => {
+        console.log(a)
+        alert("已開始爬取")
+      },
+        error => {
+          alert(error.message)
+        }
+      )
+    } else {
+      alert("不符合網址格式")
+      return;
+    }
+
 
   }
 
--- WebSite/src/app/pages/welcome/welcome.module.ts
+++ WebSite/src/app/pages/welcome/welcome.module.ts
@@ -7,12 +7,16 @@ import { WelcomeService } from './welcome.service';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports: [WelcomeRoutingModule,
     NzInputModule,NzButtonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NzTableModule,
+    CommonModule
 
   ],
   declarations: [WelcomeComponent],
