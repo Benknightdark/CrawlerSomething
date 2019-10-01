e428a253691a3888065da7a7287509bce5e5e411
Ben Kung <ben_kung@infofab.com>
2019-10-01 17:09:29 +0800

    OK

--- WebAPI/src/Api/ITHomeApi.py
+++ WebAPI/src/Api/ITHomeApi.py
@@ -6,12 +6,26 @@ from bson.json_util import dumps
 from  html import escape
 from DB import DataBase
 from config import get_env
-     
+import pika
+  
         
 ithome_blueprint = Blueprint('ithome', __name__)
 
-
 @ithome_blueprint.route('/list')
 def Get():
+    print(get_env().RABBITMQCONNECTSTRING)
     data = DataBase.ListCollection(get_env().CONNECTSTRING)
     return Response(dumps(data), mimetype='application/json')
+
+@ithome_blueprint.route('/',methods=['POST'])
+def Post(): 
+    content = request.get_json(silent=True)
+    parameters= pika.URLParameters(get_env().RABBITMQCONNECTSTRING)
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+    channel.queue_declare(queue='crawl-url')
+    channel.basic_publish(exchange='',
+                        routing_key='crawl-url',
+                        body=content['url'])
+    connection.close()
+    return Response("OK", mimetype='application/json')
--- WebAPI/src/config.py
+++ WebAPI/src/config.py
@@ -3,18 +3,20 @@ class Config(object):
     pass
 
 class ProdConfig(Config):
-    CONNECTSTRING='mongodb://root:example@localhost:1769/'
-
+    CONNECTSTRING= '' 
+    RABBITMQCONNECTSTRING =''
 class DevConfig(Config):
     DEBUG = True
     CONNECTSTRING='mongodb://root:example@localhost:1769/'
-    
+    RABBITMQCONNECTSTRING = 'amqp://rabbitmq:rabbitmq@localhost:5672/'      
 
 def get_env():
     if "ENV_VERSION" not in os.environ:
         return DevConfig
     else:
-        if os.environ['ENV_VERSION'] == 'prodoction':
+        if os.environ['ENV_VERSION'] == 'production':
+            ProdConfig.CONNECTSTRING= os.environ['MONGODBDBCONNECTSTRING']  
+            ProdConfig.RABBITMQCONNECTSTRING =os.environ['AQMPCONNECTSTRING']  
             return  ProdConfig
         else:
             return DevConfig
